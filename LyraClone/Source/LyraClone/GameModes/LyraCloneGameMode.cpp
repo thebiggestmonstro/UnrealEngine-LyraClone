2c29a388420bd0879802acac5e67565bcb38c80f
thebiggestmonstro <monstroowner@gmail.com>
2025-01-26 18:59:23 +0900

    Add GameFeature Plugin

--- LyraClone/Source/LyraClone/GameModes/LyraCloneGameMode.cpp
+++ LyraClone/Source/LyraClone/GameModes/LyraCloneGameMode.cpp
@@ -4,6 +4,7 @@
 #include "LyraCloneGameMode.h"
 
 #include "LyraCloneGameState.h"
+#include "Kismet/GameplayStatics.h"
 #include "Character/LyraCloneCharacter.h"
 #include "Player/LyraClonePlayerController.h"
 #include "Player/LyraClonePlayerState.h"
@@ -103,6 +104,14 @@ void ALyraCloneGameMode::HandleMatchAssignmentIfNotExpectingOne()
 	// - default experience
 
 	UWorld* World = GetWorld();
+
+	// 우리가 앞서, URL과 함께 ExtraArgs로 넘겼던 정보는 OptionsString에 저정되어 있다.
+	if (!ExperienceId.IsValid() && UGameplayStatics::HasOption(OptionsString, TEXT("Experience")))
+	{
+		// Experience의 Value를 가져와서, PrimaryAssetId를 생성해준다: 이때, LyraCloneExperienceDefinition의 Class 이름을 사용한다
+		const FString ExperienceFromOptions = UGameplayStatics::ParseOption(OptionsString, TEXT("Experience"));
+		ExperienceId = FPrimaryAssetId(FPrimaryAssetType(ULyraCloneExperienceDefinition::StaticClass()->GetFName()), FName(*ExperienceFromOptions));
+	}
 
 	// 우리가 앞서, URL과 함께 ExtraArgs로 넘겼던 정보는 OptionsString에 저정되어 있다.
 	if (!ExperienceId.IsValid())
