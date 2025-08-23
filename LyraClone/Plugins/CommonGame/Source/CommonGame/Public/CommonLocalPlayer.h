229e2c6aeba838ce016e526b9bfbc23e016152f8
thebiggestmonstro <monstroowner@gmail.com>
2025-08-23 13:10:01 +0900

    Add Constructor to CommonLocalPlayer

--- LyraClone/Plugins/CommonGame/Source/CommonGame/Public/CommonLocalPlayer.h
+++ LyraClone/Plugins/CommonGame/Source/CommonGame/Public/CommonLocalPlayer.h
@@ -15,6 +15,9 @@ class COMMONGAME_API UCommonLocalPlayer : public ULocalPlayer
 	GENERATED_BODY()
 	
 public:
+	/** 여기서 주목해보면 좋을 점들은 패턴이다: 우리는 여기서 FObjectInitializer::Get()과 같이 넘기지 않았다 */
+	UCommonLocalPlayer();
+
 	/** player controller가 local player에 할당(assign)되었을 경위 실행할 Delegate */
 	DECLARE_MULTICAST_DELEGATE_TwoParams(FPlayerControllerSetDelegate, UCommonLocalPlayer* LocalPlayer, APlayerController* PlayerController)
 	FPlayerControllerSetDelegate OnPlayerControllerSet;
